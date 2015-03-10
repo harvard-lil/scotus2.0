76bb26bc4e44bee2ce12536f304a35713c44fac4
Matt Phillips <mphillips@law.harvard.edu>
2015-03-10 17:55:18 -0400

    Added a normalized rot chart

--- web/js/landing.js
+++ web/js/landing.js
@@ -1,105 +1,237 @@
+var color_range = ["#0ed369", "#ff9a90", "#dddddd"];
+
+
+
+var draw_rot_vis = function() {
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear()
+        .rangeRound([height, 0]);
+
+    var color = d3.scale.ordinal()
+        .range(color_range);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(d3.format(".1s"));
+
+    var svg = d3.select(".rot-vis-container").append("svg")
+        .attr("width", "100%")
+        .attr('viewBox', '0 0 ' + String(width + margin.left + margin.right) + ' ' + String(height + margin.top + margin.bottom))
+        .attr('perserveAspectRatio', 'none')
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv("d3-data/year-rot-d3.csv", function(error, data) {
+      color.domain(d3.keys(data[0]).filter(function(key) { return key !== "Year"; }));
+
+      data.forEach(function(d) {
+        var y0 = 0;
+        d.ages = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
+        d.total = d.ages[d.ages.length - 1].y1;
+      });
+
+      //data.sort(function(a, b) { return b.total - a.total; });
+
+      x.domain(data.map(function(d) { return d.Year; }));
+      y.domain([0, d3.max(data, function(d) { return d.total; })]);
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .attr("fill", "#555")
+          .call(xAxis);
+
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .attr("fill", "#555")
+          .text("Citation to the Web");
+
+      var state = svg.selectAll(".state")
+          .data(data)
+        .enter().append("g")
+          .attr("class", "g")
+          .attr("transform", function(d) { return "translate(" + x(d.Year) + ",0)"; });
+
+      state.selectAll("rect")
+          .data(function(d) { return d.ages; })
+        .enter().append("rect")
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.y1); })
+          .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+          .style("fill", function(d) { return color(d.name); });
+
+      var legend = svg.selectAll(".legend")
+          .data(color.domain().slice().reverse())
+        .enter().append("g")
+          .attr("class", "legend")
+          .attr("transform", function(d, i) { return "translate(-550," + i * 20 + ")"; });
+
+      legend.append("rect")
+          .attr("x", width - 18)
+          .attr("width", 18)
+          .attr("height", 18)
+          .style("fill", color);
+
+      legend.append("text")
+          .attr("x", width - 24)
+          .attr("y", 9)
+          .attr("dy", ".35em")
+          .style("text-anchor", "end")
+          .text(function(d) { return d; });
+
+    });
+
+
+}
+
+var draw_rot_100_vis = function() {
+
+    var margin = {top: 20, right: 100, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear()
+        .rangeRound([height, 0]);
+
+    var color = d3.scale.ordinal()
+        .range(color_range);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(d3.format(".0%"));
+
+    var svg = d3.select(".rot-vis-100-container").append("svg")
+        .attr("width", "100%")
+        .attr('viewBox', '0 0 ' + String(width + margin.left + margin.right) + ' ' + String(height + margin.top + margin.bottom))
+        .attr('perserveAspectRatio', 'none')
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv("d3-data/year-rot-d3.csv", function(error, data) {
+      color.domain(d3.keys(data[0]).filter(function(key) { return key !== "Year"; }));
+
+      data.forEach(function(d) {
+        var y0 = 0;
+        d.ages = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
+        d.ages.forEach(function(d) { d.y0 /= y0; d.y1 /= y0; });
+      });
+
+      //data.sort(function(a, b) { return b.ages[0].y1 - a.ages[0].y1; });
+
+      x.domain(data.map(function(d) { return d.Year; }));
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis);
+
+      var state = svg.selectAll(".state")
+          .data(data)
+        .enter().append("g")
+          .attr("class", "state")
+          .attr("transform", function(d) { return "translate(" + x(d.Year) + ",0)"; });
+
+      state.selectAll("rect")
+          .data(function(d) { return d.ages; })
+        .enter().append("rect")
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.y1); })
+          .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+          .style("fill", function(d) { return color(d.name); });
+
+      var legend = svg.select(".state:last-child").selectAll(".legend")
+          .data(function(d) { return d.ages; })
+        .enter().append("g")
+          .attr("class", "legend")
+          .attr("transform", function(d) { return "translate(" + x.rangeBand() / 2 + "," + y((d.y0 + d.y1) / 2) + ")"; });
+
+      legend.append("line")
+          .attr("x2", 10);
+
+      legend.append("text")
+          .attr("x", 13)
+          .attr("dy", ".35em")
+          .text(function(d) { return d.name; });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
 $( document ).ready(function() {
 
 
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
-
-var y = d3.scale.linear()
-    .rangeRound([height, 0]);
-
-var color = d3.scale.ordinal()
-    .range(["#0ed369", "#ff9a90", "#dddddd"]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .tickFormat(d3.format(".1s"));
 
 
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-                            width = 960 - margin.left - margin.right,
-                            height = 500 - margin.top - margin.bottom;
 
-var svg = d3.select(".rot-vis-container").append("svg")
-    .attr("width", "100%")
-    .attr('viewBox', '0 0 ' + String(width + margin.left + margin.right) + ' ' + String(height + margin.top + margin.bottom))
-    .attr('perserveAspectRatio', 'none')
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("d3-data/year-rot-d3.csv", function(error, data) {
-  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "Year"; }));
 
-  data.forEach(function(d) {
-    var y0 = 0;
-    d.ages = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
-    d.total = d.ages[d.ages.length - 1].y1;
-  });
 
-  //data.sort(function(a, b) { return b.total - a.total; });
 
-  x.domain(data.map(function(d) { return d.Year; }));
-  y.domain([0, d3.max(data, function(d) { return d.total; })]);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .attr("fill", "#555")
-      .call(xAxis);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .attr("fill", "#555")
-      .text("Citation to the Web");
 
-  var state = svg.selectAll(".state")
-      .data(data)
-    .enter().append("g")
-      .attr("class", "g")
-      .attr("transform", function(d) { return "translate(" + x(d.Year) + ",0)"; });
 
-  state.selectAll("rect")
-      .data(function(d) { return d.ages; })
-    .enter().append("rect")
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.y1); })
-      .attr("height", function(d) { return y(d.y0) - y(d.y1); })
-      .style("fill", function(d) { return color(d.name); });
 
-  var legend = svg.selectAll(".legend")
-      .data(color.domain().slice().reverse())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(-550," + i * 20 + ")"; });
 
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
 
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+draw_rot_vis();
+
+draw_rot_100_vis();
+
+
+
 
 });
 
